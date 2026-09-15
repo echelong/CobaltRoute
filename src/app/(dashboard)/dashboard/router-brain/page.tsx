42026282d73c4789eabef5a46f8e42ca035cbcfb
Cobalt <alyxtron23@gmail.com>
2026-09-15 07:54:43 +0300

    feat: add coding verifier panel to Router Brain

--- src/app/(dashboard)/dashboard/router-brain/page.tsx
+++ src/app/(dashboard)/dashboard/router-brain/page.tsx
@@ -1,7 +1,13 @@
 import RouterBrainClient from "./RouterBrainClient";
+import CodingFeedbackPanel from "./CodingFeedbackPanel";
 
 export const dynamic = "force-dynamic";
 
 export default function RouterBrainPage() {
-  return <RouterBrainClient />;
+  return (
+    <>
+      <RouterBrainClient />
+      <CodingFeedbackPanel />
+    </>
+  );
 }
